ee2caf15a7b8b46fce034d6c4de493313f690ce4
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-10-12 14:37:01 +0900

    JJ test commit

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const http = require("http");
-const url = require("url");
-const path = require("path");
-const fs = require("fs");
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 const port = process.argv[2] || process.env.PORT;
 
 const mimeTypes = {
-  html: "text/html",
-  jpeg: "image/jpeg",
-  jpg: "image/jpeg",
-  png: "image/png",
-  svg: "image/svg+xml",
-  json: "application/json",
-  js: "text/javascript",
-  mp3: "audio/mpeg",
-  mp4: "video/mp4",
-  css: "text/css",
-  glb: "model/gltf-binary",
-  gltf: "model/gltf-binary",
+  html: 'text/html',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  json: 'application/json',
+  js: 'text/javascript',
+  mp3: 'audio/mpeg',
+  mp4: 'video/mp4',
+  css: 'text/css',
+  glb: 'model/gltf-binary',
+  gltf: 'model/gltf-binary',
 };
 
 const server = http.createServer(function (request, response) {
@@ -28,35 +28,35 @@ const server = http.createServer(function (request, response) {
 
   try {
     let _f = fs.statSync(filename);
-    if (_f.isDirectory()) filename += "/index.html";
+    if (_f.isDirectory()) filename += '/index.html';
   } catch (err) {
-    response.writeHead(404, { "Content-Type": "text/plain" });
-    response.write("/// " + err.errno + "\n" + err);
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('/// ' + err.errno + '\n' + err);
     response.end();
     return;
   }
 
-  fs.readFile(filename, "binary", function (err, file) {
+  fs.readFile(filename, 'binary', function (err, file) {
     if (err) {
-      response.writeHead(500, { "Content-Type": "text/plain" });
-      response.write("/// " + err.errno + "\n" + err);
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.write('/// ' + err.errno + '\n' + err);
       response.end();
       return;
     }
 
-    let mimeType = mimeTypes[filename.split(".").pop()];
+    let mimeType = mimeTypes[filename.split('.').pop()];
 
     if (!mimeType) {
-      mimeType = "text/plain";
+      mimeType = 'text/plain';
     }
 
-    response.writeHead(200, { "Content-Type": mimeType });
-    response.write(file, "binary");
+    response.writeHead(200, { 'Content-Type': mimeType });
+    response.write(file, 'binary');
     response.end();
   });
 });
 
 // server.listen(8080, function () {
 server.listen(parseInt(port, 10), function () {
-  console.log("Server is running...");
+  console.log('Server is running to', port + 'PORT');
 });
