67d0e80d8004fbf2d6c917b5e18ab6f86707ecb8
help_me_up <93499348+jaejeonglee@users.noreply.github.com>
2022-10-26 16:49:43 +0900

    jj mysql promise 사용가능 적용

--- routes/board.js
+++ routes/board.js
@@ -1,150 +1,134 @@
 const router = require('express').Router();
 const db_config = require('../dbConfig');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const pool = new mysql.createPool(db_config);
+const promisePool = pool.promise();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { currentPage, searchText, searchType } = req.body;
-    let totalCount = 0;
     let pageSize = 20;
     let queryText = '';
     if (searchType === 1) {
         queryText = ``;
     }
-    const A = pool.query(
-        `
+
+    try {
+        const execQuery = `
         select count(BOARD_ID) as totalCount from TB_BOARD
-        `,
-        (error, rows) => {
-            if (error) throw error;
-            else totalCount = rows[0].totalCount;
-        }
-    );
-    pool.query(
-        `
-        select * from (select ROW_NUMBER() over(ORDER by BOARD_ID asc) as row_num,
-        tb.*,tc.COMMENT_ID
-        from TB_BOARD tb left join TB_COMMENT tc on tb.BOARD_ID=tc.BOARD_ID) as T
-        WHERE row_num BETWEEN
-        ${totalCount - 1 * pageSize + 1} and ${totalCount - (1 - 1) * pageSize}
-        order by row_num desc
-    `,
-        (error, rows) => {
-            if (error) throw error;
-            else return res.send({ success: true, rows });
-        }
-    );
+        `;
+        const execResult = await promisePool.query(execQuery);
+        const totalCount = execResult[0][0].totalCount;
+
+        const pagingQuery = `
+        select 
+            list.* 
+        from (
+            select 
+                ROW_NUMBER() over(ORDER by BOARD_ID asc) as row_num, tb.*, tc.COMMENT_ID
+            from
+                TB_BOARD tb left join TB_COMMENT tc on tb.BOARD_ID=tc.BOARD_ID
+            ) as list
+        WHERE 
+            row_num BETWEEN
+            ${totalCount - currentPage * pageSize + 1} 
+            and 
+            ${totalCount - (currentPage - 1) * pageSize}
+            order by row_num desc
+        `;
+
+        const pagingResult = await promisePool.query(pagingQuery);
+        const boardList = pagingResult[0];
+        return res.status(200).json({ success: true, result: boardList });
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
+    }
 });
 
 router.post('/check', async (req, res) => {
     const { board_id, pswd } = req.body;
     const masterKey = process.env.MASTERKEY;
+    try {
+        if (pswd === masterKey) {
+            const execQuery = ` select * from TB_BOARD where BOARD_ID = ${board_id}`;
+            await promisePool.query(execQuery);
 
-    if (pswd === masterKey) {
-        pool.query(
-            `
-      select * from TB_BOARD where BOARD_ID = ${board_id}
-      `,
-            (error, rows) => {
-                if (error) throw error;
-                else return res.send({ success: true, masterKey: true });
+            return res.status(200).json({ success: true, masterKey: true });
+        } else {
+            const execQuery = ` select * from TB_BOARD where BOARD_ID = ${board_id} and PSWD = '${pswd}'`;
+            const execResult = await promisePool.query(execQuery);
+            const article = execResult[0][0];
+            if (article) {
+                return res
+                    .status(200)
+                    .json({ success: true, masterKey: false });
+            } else {
+                return res.status(200).json({
+                    success: false,
+                    msg: '비밀번호가 일치하지 않습니다.',
+                });
             }
-        );
-    } else {
-        pool.query(
-            `
-      select * from TB_BOARD where BOARD_ID = ${board_id} and PSWD = '${pswd}'
-      `,
-
-            (error, rows) => {
-                if (error) throw error;
-                else {
-                    if (rows.length) {
-                        return res.send({ success: true, masterKey: false });
-                    } else {
-                        return res.send({
-                            success: false,
-                            msg: '비밀번호가 일치하지 않습니다.',
-                        });
-                    }
-                }
-            }
-        );
+        }
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
     }
 });
 
 router.post('/article', async (req, res) => {
     const { board_id } = req.body;
-    pool.query(
-        `
-      select * from TB_BOARD where BOARD_ID = ${board_id}
-      `,
 
-        (error, rows) => {
-            if (error) throw error;
-            else {
-                if (rows.length) {
-                    return res.send({ success: true, rows });
-                } else {
-                    return res.send({
-                        success: false,
-                        msg: '잠시 후 다시 시도해주세요.',
-                    });
-                }
-            }
-        }
-    );
+    try {
+        const execQuery = `select * from TB_BOARD where BOARD_ID = ${board_id}`;
+        const execResult = await promisePool.query(execQuery);
+        const article = execResult[0][0];
+        return res.status(200).json({ success: true, article });
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
+    }
 });
 
 router.post('/post', async (req, res) => {
     const { board_id, title, name, pswd, contents, email, phone_number, link } =
         req.body;
-    if (board_id) {
-        pool.query(
-            `
-      update TB_BOARD set TITLE =  '${title}', WRITER = '${name}', PSWD =  '${pswd}', CONTENTS = '${contents}', EMAIL = '${email}', PHONE_NUMBER = '${phone_number}', LINK='${link}' where BOARD_ID = ${board_id}
-      `,
-            (error, rows) => {
-                if (error) throw error;
-                else
-                    return res.send({
-                        success: true,
-                        msg: '게시글이 수정되었습니다.',
-                    });
-            }
-        );
-    } else {
-        pool.query(
-            `
-      insert into TB_BOARD (TITLE, WRITER, PSWD ,CONTENTS, EMAIL, PHONE_NUMBER, REG_DATE,LINK) values ('${title}','${name}', '${pswd}','${contents}', '${email}','${phone_number}' , now(),'${link}' )
-      `,
-            (error, rows) => {
-                if (error) throw error;
-                else
-                    return res.send({
-                        success: true,
-                        msg: '게시글이 작성되었습니다.',
-                    });
-            }
-        );
+    try {
+        if (board_id) {
+            const execQuery = `
+            update TB_BOARD set TITLE =  '${title}', WRITER = '${name}', PSWD =  '${pswd}', CONTENTS = '${contents}', EMAIL = '${email}', PHONE_NUMBER = '${phone_number}', LINK='${link}' where BOARD_ID = ${board_id}
+            `;
+            await promisePool.query(execQuery);
+
+            return res
+                .status(200)
+                .json({ success: true, msg: '게시글이 수정되었습니다.' });
+        } else {
+            const execQuery = `
+            insert into TB_BOARD (TITLE, WRITER, PSWD ,CONTENTS, EMAIL, PHONE_NUMBER, REG_DATE,LINK) values ('${title}','${name}', '${pswd}','${contents}', '${email}','${phone_number}' , now(),'${link}' )
+            `;
+            await promisePool.query(execQuery);
+
+            return res
+                .status(200)
+                .json({ success: true, msg: '게시글이 작성되었습니다.' });
+        }
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
     }
 });
 
 router.post('/delete', async (req, res) => {
     const { board_id } = req.body;
-
-    pool.query(
-        `
-        delete from TB_BOARD where BOARD_ID = ${board_id}
-        `,
-        (error, rows) => {
-            if (error) throw error;
-            else
-                return res.send({
-                    success: true,
-                    msg: '게시글이 삭제되었습니다.',
-                });
-        }
-    );
+    try {
+        const execQuery = `delete from TB_BOARD where BOARD_ID = ${board_id}`;
+        await promisePool.query(execQuery);
+        return res
+            .status(200)
+            .json({ success: true, msg: '게시글이 삭제되었습니다.' });
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
+    }
 });
 module.exports = router;
--- routes/comment.js
+++ routes/comment.js
@@ -1,65 +1,63 @@
 const router = require('express').Router();
 const db_config = require('../dbConfig');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const pool = new mysql.createPool(db_config);
+const promisePool = pool.promise();
 
 router.post('/', async (req, res) => {
     const { board_id } = req.body;
-    pool.query(
-        `
-    select * from TB_COMMENT where BOARD_ID = ${board_id} and exists(select * from TB_COMMENT where BOARD_ID = ${board_id})
-        `,
-        (error, rows) => {
-            if (error) throw error;
-            if (rows.length) {
-                return res.send({ success: true, rows });
-            } else {
-                return res.send({ success: false });
-            }
+    try {
+        const execQuery = `
+        select * from TB_COMMENT where BOARD_ID = ${board_id} and exists(select * from TB_COMMENT where BOARD_ID = ${board_id})
+        `;
+        const execResult = await promisePool.query(execQuery);
+        const comment = execResult[0];
+        if (comment.length) {
+            return res.status(200).json({ success: true, comment });
+        } else {
+            return res.status(200).json({ success: false });
         }
-    );
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
+    }
 });
 
 router.post('/post', async (req, res) => {
     const { board_id, comment_id, contents, link } = req.body;
-
-    if (comment_id) {
-        pool.query(
-            `
+    try {
+        if (comment_id) {
+            const execQuery = `
             update TB_COMMENT set CONTENTS='${contents}' , LINK='${link}' where COMMENT_ID = ${comment_id}
-            `,
-            (error, rows) => {
-                if (error) throw error;
-                else return res.send({ success: true });
-            }
-        );
-    } else {
-        pool.query(
-            `
+            `;
+            await promisePool.query(execQuery);
+            return res.status(200).json({ success: true });
+        } else {
+            const execQuery = `
             insert into TB_COMMENT (CONTENTS,REG_DATE,BOARD_ID) values ('${contents}',now(), ${board_id})
-            `,
-            (error, rows) => {
-                if (error) throw error;
-                else return res.send({ success: true });
-            }
-        );
+            `;
+            await promisePool.query(execQuery);
+            return res.status(200).json({ success: true });
+        }
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
     }
 });
 
 router.post('/delete', async (req, res) => {
     const { comment_id } = req.body;
-    pool.query(
-        `
+    try {
+        const execQuery = `
         delete from TB_COMMENT where COMMENT_ID = ${comment_id}
-        `,
-        (error, rows) => {
-            if (error) throw error;
-            else
-                return res.send({
-                    success: true,
-                    msg: '답변이 삭제되었습니다.',
-                });
-        }
-    );
+        `;
+        await promisePool.query(execQuery);
+        return res
+            .status(200)
+            .json({ success: true, msg: '답변이 삭제되었습니다.' });
+    } catch (error) {
+        console.log(error, '\n', req.route.path);
+        return res.status(400).json({ success: false, error });
+    }
 });
 module.exports = router;
